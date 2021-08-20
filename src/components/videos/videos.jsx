4ac58f54e51dff5f9c0c2998759a7c6f44f9fd52
Josué Rubio <josu_mr@hotmail.com>
2021-08-20 18:34:01 +0200

    youtube for vimeo

---
+++ src/components/videos/videos.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+function Videos() {
+  return <div className='Videos'>Videos</div>;
+}
+
+export default Videos;
--- src/routing.jsx
+++ src/routing.jsx
@@ -1,23 +1,29 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link, Switch, Redirect } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  Redirect,
+} from 'react-router-dom';
 import Home from './components/home/home';
 import Galery from './components/galeria/galery';
+import Videos from './components/videos/videos';
 import Trajectory from './components/trajectory/trajectory';
 
-
-export default class Routing extends Component{
-    render() {
-        return (
-            <Router>
-                <div>
-                    <Switch>
-                        <Route path="/" component={Home} />
-                        <Route path="/galeria" component={Galery} />
-                        <Route path="/trayectoria" component={Trajectory} />
-                    </Switch>
-
-                </div>
-            </Router>
-        );
-    }
+export default class Routing extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Switch>
+            <Route path='/' component={Home} />
+            <Route path='/galeria' component={Galery} />
+            <Route path='/videos' component={Videos} />
+            <Route path='/trayectoria' component={Trajectory} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
